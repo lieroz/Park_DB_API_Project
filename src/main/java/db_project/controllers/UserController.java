0083991184909bc4576823d2dec5cb737947e7f4
lieroz <a.kamakin@icloud.com>
2017-02-24 19:46:21 +0300

    Added update user request to DB

--- src/main/java/db_project/controllers/UserController.java
+++ src/main/java/db_project/controllers/UserController.java
@@ -1,14 +1,13 @@
 package db_project.controllers;
 
 import db_project.models.UserModel;
+import db_project.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -19,9 +18,11 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
     private final JdbcTemplate jdbcTemplate;
+    private final UserService service;
 
-    public UserController(JdbcTemplate jdbcTemplate) {
+    public UserController(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        service = new UserService(jdbcTemplate);
     }
 
     @RequestMapping(value = "/user/{nickname}/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -29,17 +30,26 @@ public class UserController {
             @RequestBody UserModel user,
             @PathVariable(value = "nickname") String nickname
     ) {
-        String sql = "INSERT INTO Users (about, email, fullname, nickname) VALUES(?, ?, ?, ?);";
-        jdbcTemplate.update(sql, user.getAbout(), user.getEmail(), user.getFullname(), nickname);
-        return new ResponseEntity<>(new UserModel(user.getAbout(), user.getEmail(), user.getFullname(), nickname), HttpStatus.CREATED);
+        user.setNickname(nickname);
+        final Integer rowsAffected = service.insertUserIntoDb(user);
+
+        if (rowsAffected == 0) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        return new ResponseEntity<>(new UserModel(user), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/user/{nickname}/profile", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserModel> viewProfile(
             @PathVariable(value = "nickname") String nickname
     ) {
-        String sql = "SELECT * FROM Users WHERE nickname = ?";
-        List<UserModel> users = jdbcTemplate.query(sql, new Object[]{nickname}, UserController::readItem);
+        List<UserModel> users = service.getUserFromDb(nickname);
+
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return new ResponseEntity<>(users.get(0), HttpStatus.OK);
     }
 
@@ -48,12 +58,9 @@ public class UserController {
             @RequestBody UserModel user,
             @PathVariable(value = "nickname") String nickname
     ) {
-        String sql = "UPDATE Users SET about = ?, email = ?, fullname = ? WHERE nickname = ?;";
-        jdbcTemplate.update(sql, user.getAbout(), user.getEmail(), user.getFullname(), nickname);
-        return new ResponseEntity<>(new UserModel(user.getAbout(), user.getEmail(), user.getFullname(), nickname), HttpStatus.OK);
-    }
+        user.setNickname(nickname);
+        user = service.updateUserInfoFromDb(user);
 
-    private static UserModel readItem(ResultSet rs, int rowNum) throws SQLException {
-        return new UserModel(rs.getString("about"), rs.getString("email"), rs.getString("fullname"), rs.getString("nickname"));
+        return new ResponseEntity<>(new UserModel(user), HttpStatus.OK);
     }
 }
--- src/main/java/db_project/services/UserService.java
+++ src/main/java/db_project/services/UserService.java
@@ -1,6 +1,13 @@
 package db_project.services;
 
+import db_project.models.UserModel;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lieroz on 23.02.17.
@@ -8,4 +15,56 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserService(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public final Integer insertUserIntoDb(final UserModel user) {
+        final String sql = "INSERT INTO Users (about, email, fullname, nickname) VALUES(?, ?, ?, ?)";
+
+        return jdbcTemplate.update(sql, user.getAbout(), user.getEmail(), user.getFullname(), user.getNickname());
+    }
+
+    public final List<UserModel> getUserFromDb(final String nickname) {
+        final String sql = "SELECT * FROM Users WHERE nickname = ?";
+
+        return jdbcTemplate.query(sql, new Object[]{nickname}, UserService::readItem);
+    }
+
+    public final UserModel updateUserInfoFromDb(final UserModel user) {
+        final UserModel dbUserInfo = getUserFromDb(user.getNickname()).get(0);
+        final StringBuilder sql = new StringBuilder("UPDATE Users SET");
+        final List<Object> args = new ArrayList<>();
+
+        if (!user.getAbout().isEmpty()) {
+            sql.append(" about = ?,");
+            args.add(user.getAbout());
+            dbUserInfo.setAbout(user.getAbout());
+        }
+
+        if (!user.getEmail().isEmpty()) {
+            sql.append(" email = ?,");
+            args.add(user.getEmail());
+            dbUserInfo.setEmail(user.getEmail());
+        }
+
+        if (!user.getFullname().isEmpty()) {
+            sql.append(" fullname = ?,");
+            args.add(user.getFullname());
+            dbUserInfo.setFullname(user.getFullname());
+        }
+
+        sql.delete(sql.length() - 1, sql.length());
+        sql.append(" WHERE nickname = ?");
+        args.add(user.getNickname());
+        jdbcTemplate.update(sql.toString(), args.toArray());
+
+        return dbUserInfo;
+    }
+
+    private static UserModel readItem(ResultSet rs, int rowNum) throws SQLException {
+        return new UserModel(rs.getString("about"), rs.getString("email"), rs.getString("fullname"), rs.getString("nickname"));
+    }
 }
