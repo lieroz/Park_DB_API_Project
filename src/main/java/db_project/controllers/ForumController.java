d866d149964e1064862fc39140ecdb388aea337f
lieroz <a.kamakin@icloud.com>
2017-02-27 01:41:57 +0300

    Declared constructors for forum service and controller.

--- src/main/java/db_project/controllers/ForumController.java
+++ src/main/java/db_project/controllers/ForumController.java
@@ -1,7 +1,17 @@
 package db_project.controllers;
 
+import db_project.services.ForumService;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Created by lieroz on 27.02.17.
  */
 public class ForumController {
+    private final JdbcTemplate jdbcTemplate;
+    private final ForumService service;
+
+    public ForumController(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.service = new ForumService(jdbcTemplate);
+    }
 }
--- src/main/java/db_project/services/ForumService.java
+++ src/main/java/db_project/services/ForumService.java
@@ -1,7 +1,14 @@
 package db_project.services;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Created by lieroz on 27.02.17.
  */
 public class ForumService {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ForumService(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
