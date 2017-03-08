589b88c6c7a9761f90f1bfb7b3ba32e3d3087ee6
lieroz <a.kamakin@icloud.com>
2017-03-08 18:12:09 +0300

    Implemented dirty sorting.

--- src/main/java/db_project/controllers/ThreadController.java
+++ src/main/java/db_project/controllers/ThreadController.java
@@ -16,7 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by lieroz on 27.02.17.
@@ -161,6 +163,8 @@ public class ThreadController {
         return new ResponseEntity<>(threads.get(0), HttpStatus.OK);
     }
 
+    private static Integer markerValue = 0;
+
     @RequestMapping(value = "/posts", produces = MediaType.APPLICATION_JSON_VALUE)
     public final ResponseEntity<Test> viewThreads(
             @RequestParam(value = "limit", required = false, defaultValue = "100") final Integer limit,
@@ -169,29 +173,151 @@ public class ThreadController {
             @RequestParam(value = "desc", required = false) final Boolean desc,
             @PathVariable("slug") final String slug
     ) {
-        final StringBuilder sql = new StringBuilder("SELECT * FROM posts WHERE posts.thread = " +
-                "(SELECT threads.id FROM threads WHERE threads.slug = ?) ORDER BY posts.created");
+        if (marker != null && !Objects.equals(sort, "parent_tree")) {
+            markerValue += limit;
+        }
+
+        final StringBuilder sql = new StringBuilder();
+        Integer id = null;
+        Boolean isNumber = false;
+
+        try {
+            id = Integer.valueOf(slug);
+            isNumber = Boolean.TRUE;
+
+            if (Objects.equals(sort, "flat")) {
+                sql.append("SELECT * FROM posts WHERE posts.thread = " +
+                        "(SELECT threads.id FROM threads WHERE threads.id = ?) ORDER BY posts.created");
+
+            } else if (Objects.equals(sort, "tree")) {
+                sql.append("WITH RECURSIVE some_threads AS (SELECT * FROM posts WHERE posts.thread = " +
+                        "(SELECT threads.id FROM threads WHERE threads.id = ?)), " +
+                        "tree AS (SELECT *, array[id] AS path " +
+                        "FROM some_threads WHERE parent = 0 " +
+                        "UNION " +
+                        "SELECT " +
+                        "st.*, tree.path || st.id AS path " +
+                        "FROM tree " +
+                        "JOIN some_threads st ON st.parent = tree.id) " +
+                        "SELECT * FROM tree ORDER BY path");
+
+            } else if (Objects.equals(sort, "parent_tree")) {
+                sql.append("WITH RECURSIVE some_threads AS (SELECT * FROM posts WHERE posts.thread = " +
+                        "(SELECT threads.id FROM threads WHERE threads.id = ?)), " +
+                        "tree AS (SELECT *, array[id] AS path " +
+                        "FROM some_threads WHERE parent = 0 " +
+                        "UNION " +
+                        "SELECT " +
+                        "st.*, tree.path || st.id AS path " +
+                        "FROM tree " +
+                        "JOIN some_threads st ON st.parent = tree.id) " +
+                        "SELECT * FROM tree ORDER BY path");
+            }
+
+        } catch (NumberFormatException ex) {
+            if (Objects.equals(sort, "flat")) {
+                sql.append("SELECT * FROM posts WHERE posts.thread = " +
+                        "(SELECT threads.id FROM threads WHERE LOWER(threads.slug) = LOWER(?)) ORDER BY posts.created");
+
+            } else if (Objects.equals(sort, "tree")) {
+                sql.append("WITH RECURSIVE some_threads AS (SELECT * FROM posts WHERE posts.thread = " +
+                        "(SELECT threads.id FROM threads WHERE LOWER(threads.slug) = LOWER(?))), " +
+                        "tree AS (SELECT *, array[id] AS path " +
+                        "FROM some_threads WHERE parent = 0 " +
+                        "UNION " +
+                        "SELECT " +
+                        "st.*, tree.path || st.id AS path " +
+                        "FROM tree " +
+                        "JOIN some_threads st ON st.parent = tree.id) " +
+                        "SELECT * FROM tree ORDER BY path");
+
+            } else if (Objects.equals(sort, "parent_tree")) {
+                sql.append("WITH RECURSIVE some_threads AS (SELECT * FROM posts WHERE posts.thread = " +
+                        "(SELECT threads.id FROM threads WHERE LOWER(threads.slug) = LOWER(?))), " +
+                        "tree AS (SELECT *, array[id] AS path " +
+                        "FROM some_threads WHERE parent = 0 " +
+                        "UNION " +
+                        "SELECT " +
+                        "st.*, tree.path || st.id AS path " +
+                        "FROM tree " +
+                        "JOIN some_threads st ON st.parent = tree.id) " +
+                        "SELECT * FROM tree ORDER BY path");
+            }
+        }
 
         if (desc == Boolean.TRUE) {
             sql.append(" DESC");
         }
 
-        sql.append(" LIMIT ?");
-
         final List<PostModel> posts = jdbcTemplate.query(
                 sql.toString(),
-                new Object[]{slug, limit},
+                isNumber ? new Object[]{id} : new Object[]{slug},
                 PostService::read
         );
 
-        return new ResponseEntity<>(new Test(posts), HttpStatus.OK);
+        if (posts.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Integer temp = 0;
+
+        if (Objects.equals(sort, "parent_tree")) {
+            Integer zeroCount = 0;
+
+            if (markerValue >= posts.size() && marker != null) {
+                markerValue = 0;
+                return new ResponseEntity<>(new Test("some marker", new ArrayList<>()), HttpStatus.OK);
+            }
+
+            if (markerValue == posts.size()) {
+                markerValue = 0;
+            }
+
+            for (PostModel post : posts.subList(markerValue, posts.size())) {
+
+                if (zeroCount.equals(limit) && desc == Boolean.TRUE) {
+                    break;
+                }
+
+                if (zeroCount.equals(limit + 1) && (desc == Boolean.FALSE || desc == null)) {
+                    --temp;
+                    break;
+                }
+
+                if (post.getParent().equals(0)) {
+                    ++zeroCount;
+                }
+
+                ++temp;
+            }
+
+            final List<PostModel> lst = posts.subList(markerValue, markerValue + temp);
+            markerValue += temp;
+
+            return new ResponseEntity<>(new Test("some marker", lst), HttpStatus.OK);
+
+        } else {
+            temp = limit + markerValue > posts.size() ? posts.size() : limit + markerValue;
+        }
+
+        if (markerValue > posts.size()) {
+            markerValue = 0;
+            return new ResponseEntity<>(new Test("some marker", new ArrayList<>()), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new Test("some marker", posts.subList(markerValue, temp)), HttpStatus.OK);
     }
 
     public class Test {
         private List<PostModel> posts;
+        private String marker;
 
         @JsonCreator
-        public Test(@JsonProperty("posts") final List<PostModel> posts) {
+        public Test(
+                @JsonProperty("marker") final String marker,
+                @JsonProperty("posts") final List<PostModel> posts
+        ) {
+            this.marker = marker;
             this.posts = posts;
         }
 
@@ -201,6 +327,14 @@ public class ThreadController {
 
         public void setPosts(final List<PostModel> posts) {
             this.posts = posts;
+        }
+
+        public final String getMarker() {
+            return this.marker;
+        }
+
+        public void setMarker(final String marker) {
+            this.marker = marker;
         }
     }
 }
